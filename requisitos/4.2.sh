3d601ccfc2d9869cd229cf4edcf76e7adbd8bfc3
XDeadHackerX <57367532+XDeadHackerX@users.noreply.github.com>
2023-04-27 05:44:54 -0400

    Delete 4.2.sh

--- requisitos/4.2.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-echo                    
-echo "                       █░█░█ █ █▀▀ █ ▀█▀ █▀█ █▀█ █░░ █░░"
-echo "                       ▀▄▀▄▀ █ █▀░ █ ░█░ █▀▄ █▄█ █▄▄ █▄▄"
-echo "              __________________________________________________"					
-echo "                ︻デ═一  Created by: XDeadHackerX v2.0  ︻デ═一 " 
-echo
-read -p "[*] Cuando aparezca (WPA Handshake) en la parte superior derecha pulse (Ctrl + c) " ok
