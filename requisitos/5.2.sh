a3cc8d3e925cfe8275cd73037a87df014a7e5897
XDeadHackerX <57367532+XDeadHackerX@users.noreply.github.com>
2023-10-18 12:50:47 -0400

    v2.2

--- requisitos/5.2.sh
+++ requisitos/5.2.sh
@@ -9,4 +9,4 @@ echo "                       ▀▄▀▄▀ █ █▀░ █ ░█░
 echo "              __________________________________________________"					
 echo "                ︻デ═一  Created by: XDeadHackerX v2.2  ︻デ═一 " 
 echo
-read -p "[*] Cuando aparezca (WPA Handshake) en la parte superior derecha pulse (Ctrl + c) " ok
+read -p "[*] Cuando aparezca (WPA Handshake) en la parte superior derecha pulse (Ctrl + c) " Ok
